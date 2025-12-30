b20d15a984e410afc38a5df272eda16a8c9221c4
Nico Bailon <nico.bailon@gmail.com>
2025-12-29 22:56:24 -0800

    fix: remove unused useState import from use-app-state.ts

--- src/hooks/use-app-state.ts
+++ src/hooks/use-app-state.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState, useMemo } from 'react';
+import { useCallback, useEffect, useRef, useMemo } from 'react';
 import { unstable_batchedUpdates, flushSync } from 'react-dom';
 
 import { normalizePath } from '@file-ops/path';
